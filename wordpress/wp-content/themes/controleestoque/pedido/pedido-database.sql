fd2ada90dcc8cdbeee692d7bdc10c5b822266057
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2016-05-09 03:13:02 -0300

    Alteração

--- wordpress/wp-content/themes/controleestoque/pedido/pedido-database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS 'pedido' (
-  'id_pedido' varchar(10) CHARACTER SET utf8 NOT NULL,
-  'id_produto' varchar(10) CHARACTER SET utf8 NOT NULL,
-  'id_cliente' varchar(10) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY ('id_pedido')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO 'pedido' ('id_pedido', 'id_produto', 'id_cliente') VALUES ();
-
